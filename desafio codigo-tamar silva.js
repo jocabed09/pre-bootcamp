66e4a03e69861c4c7e1d1ac425762a01b3ccc5b3
tamar silva <tamar.silva2206@gmail.com>
2022-08-26 11:12:59 -0400

    desafio codigo opcional

---
+++ desafio codigo-tamar silva.js
@@ -0,0 +1,10 @@
+var alturaNiño = 49
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(){
+var parametro1 = 'Súbete, chico'
+var parametro2 = 'Lo siento, chico. Tal vez para el proximo año'
+if (alturaNiño > 52){
+    console.log(parametro1)
+}
+else {
+    console.log(parametro2)
+}
