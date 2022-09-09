cad7a0c8481212d9b601b6880e3bf022cd8129bf
tamar silva <tamar.silva2206@gmail.com>
2022-09-09 15:27:15 -0400

    commit

--- var alturaNiño = 1,49.js
+++
@@ -1,9 +0,0 @@
-var alturaNiño = 1,49
-function muestraSiElNiñoPuedeSubirALaMontañaRusa() {
-     alturaNiño > 52
-     alturaNiño < 52
-    console.log('¡Súbete, chico!')
-    /*si a altura es mayor se debera imprimir: '¡Súbete, chico!' */
-    console.log( 'lo siento, chico. Tal vez el proximo año') 
-    /*si la altura es menor se debera imprimir: 'lo siento, chico. Tal vez el proximo año' */
-}
